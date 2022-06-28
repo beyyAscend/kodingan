559778623e261197489b21f2d15d7207382e52f9
beyyAscend <100418056+beyyAscend@users.noreply.github.com>
2022-06-28 16:54:27 +0700

    .

---
+++ Javascript/Type data.js
@@ -0,0 +1,17 @@
+let user = {
+    name: {
+        first: "Harry",
+        last: "Potter"
+    },
+    age: 20, 
+    isMuggle: false,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+let user = {
+    name: {
+        first: "Harry",
+        last: "Potter"
+    },
+    age: 20, 
+    isMuggle: false,
+    stuff: ["Magic Wind", "Flying Car", "Owl"]
+}
