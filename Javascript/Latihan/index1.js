5c5222d9d91e4f6e0005da0e1d831198a627de81
beyyAscend <100418056+beyyAscend@users.noreply.github.com>
2022-07-22 12:54:56 +0700

    5

--- Javascript/Latihan/index1.js
+++ Javascript/Latihan/index1.js
@@ -1,7 +1,25 @@
-function makeACake(...rawIngredients) {
-    const ingredients = collectIngredients(rawIngredients);
-    dough = makeTheDough(ingredients);
-    pouredDough = pourDough(dough);
-    cake = bakeACake(pouredDough);
-    console.log(cake);
+const stock = {
+    coffeeBeans : 1000,
+    water : 1000,
 }
+
+const checkStock = () => {
+    return new Promise((resolve, reject) => {
+        if (stock.coffeeBeans >= 16 && stock.water >= 250) {
+            resolve("Stock masih ada, Bisa membuat kopi");
+        } else {
+            reject("Stock habis tidak bisa membuat coffe");
+        }
+    });
+};
+
+const handleSuccess = resolvedValue => {
+    console.log(resolvedValue); 
+}
+
+const handleFailure = rejectionReason => {
+    console.log(rejectionReason);
+}
+
+checkStock().then(handleSuccess,handleFailure);
+
