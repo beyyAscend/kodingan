2e198befc7db32025b59823fd425da74dd84fd05
beyyAscend <100418056+beyyAscend@users.noreply.github.com>
2022-07-23 00:15:24 +0700

    ww

--- Javascript/Latihan/index.js
+++ Javascript/Latihan/index.js
@@ -1,13 +1,15 @@
-const orderCoffe = callback => {
-    let coffee = null;
-  console.log("Sedang membuat coffe,silahkan tunggu");
-  setTimeout(() => {
-    coffee = "Kopi sudah jadi!"
-    callback(coffee);
-  }, 3000);
+function fetchUsername() {
+  return new Promise((resolve, reject) => {
+      setTimeout(() => {
+          resolve('JSUser');
+      }, 3000);
+  })
 }
 
-orderCoffe(coffe =>{
-  console.log(coffe);
-});
-
+console.log("Fetching username...");
+fetchUsername().then((value) => {
+  console.log(`You are logged in as ${value}`);
+})
+.finally(() => {
+  console.log("Welcome!");
+})
