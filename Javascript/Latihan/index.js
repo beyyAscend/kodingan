b532048b6f2aa3022795f2fab78c855188183642
beyyAscend <100418056+beyyAscend@users.noreply.github.com>
2022-07-26 16:38:57 +0700

    y

--- Javascript/Latihan/index.js
+++ Javascript/Latihan/index.js
@@ -1,10 +1,39 @@
-function main() {
-  try {
-      console.log("Fetching username...");
-      const username = await fetchUsername();
-      console.log(`You are logged in as ${username}`);
-      console.log("Welcome!");
-  } catch(e) {
-      console.log("Username tidak ditemukan");
+let score = 90;
+let result;
+
+// TODO
+
+  if (score>=90){
+
+    result = "Selamat! Anda mendapatkan nilai A.";
+
+    console.log(result);
+
+  } else if(score>=80 && score<=89){
+
+    result = "Anda mendapatkan nilai B.";
+
+    console.log(result);
+
+  } else if(score>=70 && score<=79){
+
+    result = "Anda mendapatkan nilai C.";
+
+    console.log(result);
+
+  } else if(score>=60 && score<=69){
+
+    result = "Anda mendapatkan nilai D.";
+
+    console.log(result);
+
+  } else{
+
+    result = "Anda mendapatkan nilai E.";
+
+    console.log(result);
+
   }
-}
+
+// Jangan hapus kode ini
+return result;
